02314cbb4e53c1d68f99e580c1be26280bc7eaf4
viktor-nord <viktor.karlsson@edunord.se>
2025-10-28 14:03:39 +0100

    done

--- settings.py
+++ settings.py
@@ -1,5 +1,15 @@
 class Settings:
     def __init__(self):
+        # Screen
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.fullscreen = False
+        # Ship
+        self.ship_speed = 7.5
+        # Bullet
+        self.bullet_speed = 2.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
--- ship.py
+++ ship.py
@@ -2,11 +2,30 @@ import pygame
 
 class Ship:
     def __init__(self, ai_game):
+        self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.image = pygame.image.load('images/player.bmp')
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midleft = self.screen_rect.midleft
+        self.y = float(self.rect.y)
+        # maybe change to movement null, right or left
+        self.moving_up = False
+        self.moving_down = False
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        self.rect.y = self.y
+
+    def update2(self):
+        if self.moving_up and self.rect.top < self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom > 0:
+            self.y += self.settings.ship_speed
+        self.rect.y = self.y
