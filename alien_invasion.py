2397888d1c0a52c5980b9c2bdc33aa1b89c73e8c
viktor-nord <viktor.karlsson@edunord.se>
2025-10-28 14:06:32 +0100

    change

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,33 +2,74 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        if self.settings.fullscreen:
+            wh = (0, 0)
+            f = pygame.FULLSCREEN
+        else:
+            wh = (self.settings.screen_width, self.settings.screen_height)
+            f = 0
+        self.screen = pygame.display.set_mode(wh, f)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
+    #Helper Methods
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-    
+            # Modified
+            elif event.type == pygame.KEYDOWN:
+                self._check_key_events(event.key, True)
+            elif event.type == pygame.KEYUP:
+                self._check_key_events(event.key, False)
+                 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         pygame.display.flip()
+
+    # Modified
+    def _check_key_events(self, key, is_key_down):
+        if key == pygame.K_RIGHT:
+            self.ship.moving_right = is_key_down
+        elif key == pygame.K_LEFT:
+            self.ship.moving_left = is_key_down
+        elif key == pygame.K_q:
+            sys.exit()
+        elif key == pygame.K_SPACE and is_key_down: 
+            self._fire_bullet()
+    
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 
--- ship.py
+++ ship.py
@@ -2,11 +2,24 @@ import pygame
 
 class Ship:
     def __init__(self, ai_game):
+        self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.image = pygame.image.load('images/player.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.y -= 10
+        self.x = float(self.rect.x)
+        # maybe change to movement null, right or left
+        self.moving_right = False
+        self.moving_left = False
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
