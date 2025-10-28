645f8d61e0c48ba6df5d575afa3b3c6137c91eda
viktor-nord <viktor.karlsson@edunord.se>
2025-10-28 09:09:16 +0100

    adding ship code + img

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,7 @@
 import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -13,15 +12,25 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
